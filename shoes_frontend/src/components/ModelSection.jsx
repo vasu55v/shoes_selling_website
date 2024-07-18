d05620c1765367bcf669cc075bdbd3ff8a5eddda
vasu55v <vasuvirani55@gmail.com>
2024-07-18 17:50:17 +0530

    added model section

--- shoes_frontend/src/components/ModelSection.jsx
+++ shoes_frontend/src/components/ModelSection.jsx
@@ -3,27 +3,29 @@ import '../styles/modelSection.css'
 import shoes_model from '../assets/shoes_model.jpeg'
 import shoes_model2 from '../assets/shoes_model2.jpeg'
 import nike_model from '../assets/nike_model2.jpeg'
-import nike_model2 from '../assets/nike_model3.jpeg'
+import nike_model2 from '../assets/nike_model5.jpeg'
 
 
 const ModelSection = () => {
   return (
+    <center>
     <div className='model_section_container'>
         
         <div className='image_container'>
-             <img src={shoes_model} alt='image' />
-             <img src={shoes_model2} alt='image' />
+             <img src={shoes_model}  className="img1" alt='image' />
+             <img src={shoes_model2} className="img2" alt='image' />
         </div>
 
         <div className='second_image_container'>
             <div className='inner_second_image_container'>
-               <img src={nike_model} alt='image' />
+               <img src={nike_model} className="img3" alt='image' />
                <a href='#'>All product</a>
-               </div>
-               <img src={nike_model2} alt='image' />
+            </div>
+               <img src={nike_model2} className="img4" alt='image' />
 
         </div>
     </div>
+    </center>
   )
 }
 
