3b12fc9e9771cf0014c234248c7896720704a7ae
vasu55v <vasuvirani55@gmail.com>
2024-12-13 19:21:08 +0530

    lenis scroll effect added

--- shoes_frontend/src/components/Home.jsx
+++ shoes_frontend/src/components/Home.jsx
@@ -9,12 +9,34 @@ import Footer from './Footer.jsx'
 import Cookies from "universal-cookie"
 import { jwtDecode } from 'jwt-decode';
 import api from '../../Api'
+import Lenis from '@studio-freight/lenis';
 
 
 const Home = () => {
     // console.log(Cookies.get("vendor_id"))
 
- 
+    useEffect(() => {
+      // Initialize Lenis
+      const lenis = new Lenis({
+        duration: 1.5,   // Adjust scroll duration
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Custom easing function
+        smooth: true,    // Enable smooth scroll
+      });
+  
+      // Scroll Update Function
+      const onScroll = (time) => {
+        lenis.raf(time);
+        requestAnimationFrame(onScroll);
+      };
+  
+      requestAnimationFrame(onScroll);
+  
+      return () => {
+        // Cleanup on component unmount
+        lenis.destroy();
+      };
+    }, []);
+
   return (
     <>
     <Navbar />
